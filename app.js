64705daf84b49c13a295d13ff1575235a8cafaae
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-16 13:04:23 +0800

    短網址產生器頁面排版完成

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send(`there`)
+  res.render('index')
 })
 
 app.listen(PORT, () => {
